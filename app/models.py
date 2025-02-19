789357343f8b1002a21800fd92cb7b9da7ff54d2
Jhonatan Ibarra <ibarra.jhonatann@gmail.com>
2025-02-18 22:08:45 -0500

    Add Pydantic models for query and user management

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import List
+
+class QueryModel(BaseModel):
+    queryAsk: str
+
+class SuggestionResponse(BaseModel):
+    queryAsk: str
+    responseSuggestion: str
+
+class User(BaseModel):
+    id: str
+    username: str
+    password: str
+    role: str
+
+class Token(BaseModel):
+    idUser: str
+    role: str
+
+class  SuggestionBaseItem(BaseModel):
+    question: str
+    answer: str
+
+class HistoryEntry(BaseModel):
+    query: str
+    suggestion: str
